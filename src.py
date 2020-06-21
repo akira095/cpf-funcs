20bbd9f9a7309b94bb815dac264c8b3311d4c380
Artur Rabelo <artur.rabelo05@gmail.com>
2020-06-21 15:44:50 -0300

    Criado arquivo teste

--- src.py
+++ src.py
@@ -38,24 +38,3 @@ def gen():
 
 	result = ''.join(map(str, cpf))
 	return result
-
-
-if __name__ == '__main__':
-	
-	option = int(input('1 - Validar CPF\n'
-					   '2 - Gerar CPF válido\n'
-					   '>>> '))
-	
-	if option == 1:
-		number = input('Digite o CPF: ').strip()
-		if validate(number):
-			print('CPF válido.')
-		else:
-			print('CPF inválido.')
-
-	elif option == 2:
-		cpf = gen()
-		print(f'CPF gerado automaticamente: {cpf[0:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}')
-		
-	else:
-		print('Inválido.')
